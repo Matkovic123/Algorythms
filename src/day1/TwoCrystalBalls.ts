6ebf15ceb2d0b73015ea1c81b73aecfef0b6f6d7
Toni Matkovic <toni.matkovic0@gmail.com>
2023-03-14 18:28:38 +0100

    Compared Primeagen's and my solution and improved mine, both are equally quick

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,20 +1,43 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const t0 = performance.now();
+    // const diff = Math.floor(Math.sqrt(breaks.length));
+
+    // let i = diff
+    // for (; i < breaks.length; i += diff) {
+    //     if (breaks[i]) {
+    //         break;
+    //     }
+    // }
+
+    // i -= diff;
+
+    // for (let j = 0; j <= diff && i < breaks.length; i++, j++) {
+    //     if (breaks[i]) {
+    //         const t1 = performance.now()
+    //         console.log(`Time it takes to run the function: ${t1 - t0} ms`)
+    //         //     Time it takes to run the function: 0.0822560042142868 ms
+    //         return i;
+    //     }
+    // }
+    // return -1;
+
     let lo = 0;
     let hi = breaks.length;
+    const diff = Math.floor(Math.sqrt(hi - lo));
     do {
-        const diff = Math.floor(Math.sqrt(hi-lo));
-        if(breaks[lo] === false) {
-            lo+=diff;
-        }
-        else if(breaks[lo] === true) {
+        if (breaks[lo] === false) {
+            lo += diff;
+        } else if (breaks[lo] === true) {
             hi = lo;
-            lo-=diff;
-            while(breaks[lo] === false) {
+            lo -= diff;
+            while (breaks[lo] === false) {
                 lo++;
             }
+            const t1 = performance.now();
+            console.log(`Time it takes to run the function: ${t1 - t0} ms`);
+            //Time it takes to run Primeagen function: 0.0822560042142868 ms
             return lo;
         }
     } while (hi > lo);
     return -1;
-
 }
