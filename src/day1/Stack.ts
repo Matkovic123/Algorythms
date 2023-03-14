009ed144a90c91f47358637ec2f4cb03e8402843
Toni Matkovic <toni.matkovic0@gmail.com>
2023-03-14 21:36:16 +0100

    Implemented a stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,35 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
-
-}
+        this.length++;
+        if (!this.head) {
+            this.head = { value: item };
+            return;
+        }
+        const newItem = { value: item, next: this.head };
+        this.head = newItem;
+    }
     pop(): T | undefined {
-
-}
+        if (!this.head) {
+            return undefined;
+        }
+        this.length--;
+        const oldHead = this.head;
+        this.head = this.head.next;
+        return oldHead.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
