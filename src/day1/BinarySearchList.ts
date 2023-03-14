ab740a4b0e547abd6560d77ac501236d4413789a
Toni Matkovic <toni.matkovic0@gmail.com>
2023-03-14 17:27:35 +0100

    Implemented binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+    let low = 0;
+    let high = haystack.length
+    do {
+        const mid = Math.floor(low + (high-low) / 2)
+        if(haystack[mid] === needle) {
+            return true;
+        }
+        if(haystack[mid] > needle) {
+            high = mid;
+        }
+        if(haystack[mid] < needle) {
+            low = mid+1
+        }
+    } while(low < high)
+
+    return false;
 }
