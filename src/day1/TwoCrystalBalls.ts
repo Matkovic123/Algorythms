b4605f791914deb9873bca999f12586eea26cf25
Toni Matkovic <toni.matkovic0@gmail.com>
2023-03-14 18:17:02 +0100

    My impl of TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let lo = 0;
+    let hi = breaks.length;
+    do {
+        const diff = Math.floor(Math.sqrt(hi-lo));
+        if(breaks[lo] === false) {
+            lo+=diff;
+        }
+        else if(breaks[lo] === true) {
+            hi = lo;
+            lo-=diff;
+            while(breaks[lo] === false) {
+                lo++;
+            }
+            return lo;
+        }
+    } while (hi > lo);
+    return -1;
 
 }
