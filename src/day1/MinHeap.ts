d923e10c386e945c1d6e4651b1ec27c4cf4330ae
Toni Matkovic <toni.matkovic0@gmail.com>
2023-03-15 20:47:04 +0100

    Implemented MinHeap

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -1,15 +1,76 @@
+import linear_search from "./LinearSearchList";
+
 export default class MinHeap {
     public length: number;
-
-    
+    private data: number[];
 
     constructor() {
+        this.length = 0;
+        this.data = [];
     }
 
     insert(value: number): void {
-
-}
+        this.data[this.length] = value;
+        this.heapifyUp(this.length);
+        this.length++;
+    }
     delete(): number {
+        // pop / poll
+        if (this.length === 0) {
+            return -1;
+        }
+        const out = this.data[0];
+        this.length--;
+        if (this.length === 0) {
+            this.data = [];
+            return out;
+        }
+        this.data[0] = this.data[this.length];
+        this.heapifyDown(0);
+        return out;
+    }
+    private heapifyUp(idx: number): void {
+        if (idx === 0) {
+            return;
+        }
+        const p = this.parent(idx);
+        const parentV = this.data[p];
+        const v = this.data[idx];
 
-}
+        if (parentV > v) {
+            this.data[idx] = parentV;
+            this.data[p] = v;
+            this.heapifyUp(p);
+        }
+    }
+
+    private heapifyDown(idx: number): void {
+        const lIdx = this.getLeftChild(idx);
+        const rIdx = this.getRightChild(idx);
+        if (idx >= this.length || lIdx >= this.length) {
+            return;
+        }
+        const v = this.data[idx];
+        const lV = this.data[lIdx];
+        const rV = this.data[rIdx];
+        if (lV > rV && v > rV) {
+            this.data[idx] = rV;
+            this.data[rIdx] = v;
+            this.heapifyDown(rIdx);
+        } else if (rV > lV && v > lV) {
+            this.data[idx] = lV;
+            this.data[lIdx] = v;
+            this.heapifyDown(lIdx);
+        }
+    }
+
+    private parent(idx: number): number {
+        return Math.floor((idx - 1) / 2);
+    }
+    private getLeftChild(idx: number): number {
+        return idx * 2 + 1;
+    }
+    private getRightChild(idx: number): number {
+        return idx * 2 + 2;
+    }
 }
