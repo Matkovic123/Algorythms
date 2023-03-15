2746e65dab92dee3966fca92cbf3c336ae16b79e
Toni Matkovic <toni.matkovic0@gmail.com>
2023-03-15 17:29:53 +0100

    Implemented depth first binary tree search algorythms

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,19 @@
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if(!curr) {
+        return path;
+    }
+    // pre recursion step
+    path.push(curr.value);
+    
+    //recursion step
+    walk(curr.left, path);
+    walk(curr.right, path);
+    
+    //post recursion step
+    return path;
+}
+
 export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 
 }
